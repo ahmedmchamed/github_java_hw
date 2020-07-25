ab38c1c1136d57d52fccdfd60abc74a9e84f68c2
Ahmed Hamed <tunashark@gmail.com>
2020-07-25 10:44:26 +0100

    finished repository constructor

--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -2,11 +2,16 @@ import java.util.ArrayList;
 
 public class Repository {
 
-    private String name;
-    private String description;
-    private RepositoryType repositoryType;
+    private String repoName;
+    private String repoDescription;
+    private RepositoryType repoType;
     private ArrayList<Commit> commits;
 
-    
+    public Repository(String repoName, String repoDescription, RepositoryType repoType) {
+        this.repoName = repoName;
+        this.repoDescription = repoDescription;
+        this.repoType = repoType;
+        this.commits = new ArrayList<Commit>();
+    }
 
 }
