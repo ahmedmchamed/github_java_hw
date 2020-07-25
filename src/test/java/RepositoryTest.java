8db48255d6b19b5ea70cf0fd982837e6c08da630
Ahmed Hamed <tunashark@gmail.com>
2020-07-25 12:29:27 +0100

    return commit by id test passing

--- src/test/java/RepositoryTest.java
+++ src/test/java/RepositoryTest.java
@@ -1,18 +1,27 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class RepositoryTest {
 
-    private Commit commit;
+    private Commit initialCommit;
+    private Commit finalCommit;
     private Repository repo;
 
     @Before
     public void before() {
-        commit = new Commit("Initial commit", CommitUniqueId.UNIQUEID.createUniqueId());
+        initialCommit = new Commit("Initial commit", CommitUniqueId.UNIQUEID.createUniqueId());
+        finalCommit = new Commit("Final commit", CommitUniqueId.UNIQUEID.createUniqueId());
         repo = new Repository("GitHub repo", "CodeClan version of GitHub", RepositoryType.PUBLIC);
+        repo.addCommitToRepo(initialCommit);
+        repo.addCommitToRepo(finalCommit);
     }
 
-//    @Test
-//    public void
+    @Test
+    public void canGetCommitById() {
+        Commit result = repo.getCommitById(finalCommit.returnUniqueId());
+        assertEquals("Final commit", result.returnDescription());
+    }
 
 }
