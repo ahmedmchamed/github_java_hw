ab50e18ef833c003034213d1d0160e095fb8c2fd
Ahmed Hamed <tunashark@gmail.com>
2020-07-25 23:46:06 +0100

    start work on Runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,9 +1,18 @@
 public class Runner {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to GitClan!\n");
+        System.out.println("You can use '-h' to see a list of all the options.");
 
+        for (int i = 0; i < args.length; i++)
+        switch (args[i].charAt(0)) {
+            case '-':
+                String option = args[i].toLowerCase().toString();
+                if (option.equals("-commit")) {
+                    System.out.println("Enter your commit description.\n");
 
-
+                }
+        }
     }
 
 }
