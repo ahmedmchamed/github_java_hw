94d97e21b8b99b77f2e1b8b19734e304e3ff54bb
Ahmed Hamed <tunashark@gmail.com>
2020-07-25 10:40:45 +0100

    created repository class/enum

---
+++ src/main/java/Repository.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Repository {
+
+    private String name;
+    private String description;
+    private RepositoryType repositoryType;
+    private ArrayList<Commit> commits;
+
+    
+
+}
