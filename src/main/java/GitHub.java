6e969663fd1b1b65d16cb38a7281e553637587ac
Ahmed Hamed <tunashark@gmail.com>
2020-07-27 00:28:41 +0100

    fleshed out GitHub class

--- src/main/java/GitHub.java
+++ src/main/java/GitHub.java
@@ -1,3 +1,28 @@
-//public class GitHub {
-//
-//}
+import java.util.ArrayList;
+
+public class GitHub {
+
+    private ArrayList<GitHubAccount> gitAccountsList;
+
+    public GitHub () {
+        this.gitAccountsList = new ArrayList<GitHubAccount>();
+    }
+
+    public void addGitAccountToList(GitHubAccount gitAccount) {
+        this.gitAccountsList.add(gitAccount);
+    }
+
+    public ArrayList<GitHubAccount> getGitAccountsList() {
+        return this.gitAccountsList;
+    }
+
+    public GitHubAccount getGitAccountByUsername(String username) {
+        for (GitHubAccount accountToFind : this.gitAccountsList) {
+            if (accountToFind.getUsername().equals(username)) {
+                return accountToFind;
+            }
+        }
+        return null;
+    }
+
+}
